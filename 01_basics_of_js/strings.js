b398b772572bb8fe3aa0d010b05d2b08a1ce99ab
Aryan Sangwan <120324757+Sangwanaryan@users.noreply.github.com>
2024-12-01 12:11:05 +0000

    Updated Strings in JS

--- 01_basics_of_js/strings.js
+++ 01_basics_of_js/strings.js
@@ -3,3 +3,25 @@ const name = "Aryan"
 const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repocount is ${repoCount}`)
+
+const gameName = new String ("Aryan Sangwan")
+
+console.log(`And the game name is ${gameName}`);
+
+// Length of function can be measured by no of arguments a function have.
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+
+// trim will remove white spaces from a string 
+
+const url = "https://aryan.com/aryan%20sangwan"
+
+console.log(url.replace('%20', '-'))
+
+// Convert string to array based on dash(-) using SPLIT METHOD
+
+const str = "was-ist-dein-beruf-wie-heiben-sie"
+
+console.log(str.split('-'));
